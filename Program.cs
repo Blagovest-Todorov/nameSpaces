5c9b86edce2a219c3d399d10f64e945f686be698
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-29 10:49:07 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _10.NamespacesGroupingClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //namespaces, are sth that groupes /organizzing/ logically  by creteria  the classes;
+            //namespaces are used to organize the code into alogical bloakcs
+            //Namspaces can group classes, structures, interfaces, ....other namspaces, 
+            //when we want to use a given class we just use Namespace;
+
+            //if we want to use BigInterger
+            //we add  :  using System.Numerics;
+            //example the class   Array is a part of nameSpace : using System.Array
+            
+        }
+    }
+}
